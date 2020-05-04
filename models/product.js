0aee5f51348b5c4651bdaafc18e511e61594665c
supernovalx <hoanganhgo9@gmail.com>
2020-05-04 21:50:15 +0700

    clean up

--- models/product.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require( 'mongoose' );
-
-var productSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    description: String,
-});
-
-mongoose.model('Product', productSchema);
